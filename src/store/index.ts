b752ff52a81a9675df3f11bf545f4f0ef21b955c
Александр Буланов <alexbulanovgoodmail@gmail.com>
2026-02-14 14:25:18 +0300

    Установлен zustand, создано хранилище для выбранных настроек

---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { create } from 'zustand'
+
+const useMacbookStore = create(set => ({
+  color: '#2e2c2e',
+  setColor: (color: string) => set({ color }),
+
+  scale: 0.08,
+  setScale: (scale: number) => set({ scale }),
+
+  texture: '/videos/feature-1.mp4',
+  setTexture: (texture: string) => set({ texture }),
+
+  reset: () => set({ color: '#2e2c2e', scale: 0.08, texture: '/videos/feature-1.mp4' }),
+}))
+
+export default useMacbookStore
