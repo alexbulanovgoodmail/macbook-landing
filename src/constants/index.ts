3920dcb554195db21b237d8f11d21e448e0de580
Александр Буланов <alexbulanovgoodmail@gmail.com>
2026-02-17 17:17:00 +0300

    Добавлены константы для изображения и анимации блока представления

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -33,3 +33,51 @@ export const noChangeParts = [
   'Object_21',
   'Object_10',
 ]
+
+export const performanceImages = [
+  { id: 'p1', src: '/images/performance1.png' },
+  { id: 'p2', src: '/images/performance2.png' },
+  { id: 'p3', src: '/images/performance3.png' },
+  { id: 'p4', src: '/images/performance4.png' },
+  { id: 'p5', src: '/images/performance5.jpg' },
+  { id: 'p6', src: '/images/performance6.png' },
+  { id: 'p7', src: '/images/performance7.png' },
+]
+
+export const performanceImgPositions = [
+  {
+    id: 'p1',
+    left: 5,
+    bottom: 65,
+  },
+  {
+    id: 'p2',
+    right: 10,
+    bottom: 60,
+  },
+  {
+    id: 'p3',
+    right: -5,
+    bottom: 45,
+  },
+  {
+    id: 'p4',
+    right: -10,
+    bottom: 0,
+  },
+  {
+    id: 'p5',
+    left: 20,
+    bottom: 50,
+  },
+  {
+    id: 'p6',
+    left: 2,
+    bottom: 30,
+  },
+  {
+    id: 'p7',
+    left: -5,
+    bottom: 0,
+  },
+]
